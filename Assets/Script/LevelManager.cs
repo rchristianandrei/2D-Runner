cb56cd936a0507c7f445a4ba2712f7817d1c02c7
Christian Andrei Reyes <rchristianandrei@gmail.com>
2023-04-19 18:33:26 +0800

    add win lose screen

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -10,12 +10,20 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private float backgroundSpeed = 2;
     [SerializeField] private float groundSpeed = 3;
 
+    [SerializeField] private float MaxProgress;
+    private float currentProgress = 0;
+
     public float enemySpeed = 3;
 
     // Level Prefabs
     [SerializeField] private GameObject backgroundPrefab;
     [SerializeField] private GameObject groundPrefab;
     [SerializeField] private GameObject playerPrefab;
+
+    // References
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
+    [SerializeField] private GameObject gameplayScreen;
 
     // Flags
     public bool isGameOver { get; set; } = false;
@@ -44,5 +52,29 @@ public class LevelManager : MonoBehaviour
         controller.attackRate = playerAttackRate;
     }
 
+    private void Update()
+    {
+        currentProgress += Time.deltaTime;
 
+        if (currentProgress >= MaxProgress)
+        {
+            isGameOver = true;
+
+            // You Won Screen
+            Win();
+        }
+    }
+
+    public void Win()
+    {
+        gameplayScreen.SetActive(false);
+        winScreen.SetActive(true);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>().enabled = false;
+    }
+
+    public void Gameover()
+    {
+        gameplayScreen.SetActive(false);
+        loseScreen.SetActive(true);
+    }
 }
